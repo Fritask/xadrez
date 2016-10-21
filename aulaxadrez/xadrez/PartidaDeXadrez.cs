d288909b7e7976ec4071b013757348db81a0ece2
Fritask <fritas.series@gmail.com>
2016-10-21 14:40:28 -0200

    Jogada especial roque pequeno e roque grande

--- aulaxadrez/xadrez/PartidaDeXadrez.cs
+++ aulaxadrez/xadrez/PartidaDeXadrez.cs
@@ -34,6 +34,25 @@ namespace xadrez
             if (pecaCapturada != null) {
                 capturadas.Add(pecaCapturada);
             }
+
+            // #jogadaespecial roque pequeno
+            if(p is Rei && destino.coluna == origem.coluna + 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQteMovimentos();
+                tab.colocarPeca(T, destinoT);
+            }
+
+            // #jogadaespecial roque grande
+            if (p is Rei && destino.coluna == origem.coluna - 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQteMovimentos();
+                tab.colocarPeca(T, destinoT);
+            }
+
             return pecaCapturada;
         }
 
@@ -45,6 +64,23 @@ namespace xadrez
                 capturadas.Remove(pecaCapturada);
             }
             tab.colocarPeca(p, origem);
+
+            // #jogadaespecial roque pequeno
+            if(p is Rei && destino.coluna == origem.coluna + 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQteMovimentos();
+                tab.colocarPeca(T, origemT);
+            }
+            // #jogadaespecial roque grande
+            if (p is Rei && destino.coluna == origem.coluna - 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQteMovimentos();
+                tab.colocarPeca(T, origemT);
+            }
         }
 
         public void realizaJogada(Posicao origem, Posicao destino) {
@@ -186,7 +222,7 @@ namespace xadrez
             colocarNovaPeca('b', 1, new Cavalo(tab, cor.Branca));
             colocarNovaPeca('c', 1, new Bispo(tab, cor.Branca));
             colocarNovaPeca('d', 1, new Dama(tab, cor.Branca));
-            colocarNovaPeca('e', 1, new Rei(tab, cor.Branca));
+            colocarNovaPeca('e', 1, new Rei(tab, cor.Branca, this));
             colocarNovaPeca('f', 1, new Bispo(tab, cor.Branca));
             colocarNovaPeca('g', 1, new Cavalo(tab, cor.Branca));
             colocarNovaPeca('h', 1, new Torre(tab, cor.Branca));
@@ -203,7 +239,7 @@ namespace xadrez
             colocarNovaPeca('b', 8, new Cavalo(tab, cor.Preta));
             colocarNovaPeca('c', 8, new Bispo(tab, cor.Preta));
             colocarNovaPeca('d', 8, new Dama(tab, cor.Preta));
-            colocarNovaPeca('e', 8, new Rei(tab, cor.Preta));
+            colocarNovaPeca('e', 8, new Rei(tab, cor.Preta, this));
             colocarNovaPeca('f', 8, new Bispo(tab, cor.Preta));
             colocarNovaPeca('g', 8, new Cavalo(tab, cor.Preta));
             colocarNovaPeca('h', 8, new Torre(tab, cor.Preta));
--- aulaxadrez/xadrez/Peao.cs
+++ aulaxadrez/xadrez/Peao.cs
@@ -36,12 +36,12 @@ namespace xadrez {
                 }
 
                 pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
-                if (tab.posicaoValida(pos) && livre(pos)) {
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
 
                 pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
-                if (tab.posicaoValida(pos) && livre(pos)) {
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
             }
@@ -57,12 +57,12 @@ namespace xadrez {
                 }
 
                 pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
-                if (tab.posicaoValida(pos) && livre(pos)) {
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
 
                 pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
-                if (tab.posicaoValida(pos) && livre(pos)) {
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
             }
